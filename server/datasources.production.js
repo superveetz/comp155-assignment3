218e9129e3d0ab45417cb55121255be8b1e5b616
Alexander.DiVito <alexander.divito@student.ufv.ca>
2016-11-27 21:09:27 -0800

    added mongodb cartridge to openshift

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -5,7 +5,7 @@ module.exports = {
     "host": process.env.OPENSHIFT_MONGODB_DB_HOST,
     "port": process.env.OPENSHIFT_MONGODB_DB_PORT,
     "url": "",
-    "database": "Ass3-Web-Bank",
+    "database": "comp155assignment3",
     "password": process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
     "name": "mongoDs",
     "user": process.env.OPENSHIFT_MONGODB_DB_USERNAME,
